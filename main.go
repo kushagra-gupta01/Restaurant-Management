3f5fc7743fde84146ab33f03721753dd0f7aedb9
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-09-04 12:55:49 +0530

    main file & setup done

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	"github.com/kushagra-gupta01/Restaurant-Management/database"
+	"github.com/kushagra-gupta01/Restaurant-Management/middleware"
+	"github.com/kushagra-gupta01/Restaurant-Management/routes"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Colection = database.OpenCollection(database.Client, "food")
+
+func main(){
+	port :=os.Getenv("PORT")
+	if port ==""{
+		port = "8000"
+	}
+	router :=gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.InvoiceRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.TableRoutes(router)
+
+	router.Run(":" +port)
+}
