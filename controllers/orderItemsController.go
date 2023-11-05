6ad57015efc1d7cabfb2cc5a2dccf5ac47860632
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-11-05 12:08:58 +0530

    orderItem controller almost done: only Items by order function left

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 	"time"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/kushagra-gupta01/Restaurant-Management/database"
 	"github.com/kushagra-gupta01/Restaurant-Management/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type OrderItemPack struct{
@@ -69,12 +71,85 @@ func CreateOrderItem() gin.HandlerFunc{
 
 		order.Order_Date,_ =time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
 		
+		orderItemsToBeInserted := []interface{}{}
+		order.Table_id = orderItemPack.Table_id
+		order_id := OrderItemOrderCreator(order)
+
+		for _,orderItem := range orderItemPack.Order_items{
+			orderItem.Order_id = order_id
+
+			ValidationErr := validate.Struct(orderItem)
+			if ValidationErr !=nil{
+				c.JSON(http.StatusBadRequest,gin.H{"error":ValidationErr.Error()})
+				return
+			}
+
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.Created_at,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+			orderItem.Updated_at,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+			orderItem.Order_item_id = orderItem.ID.Hex()
+
+			var num = toFixed(*orderItem.Unit_price,2)
+			orderItem.Unit_price = &num
+			orderItemsToBeInserted = append(orderItemsToBeInserted, orderItem)
+		}
+
+		insertedOrderItems, err:= orderItemCollection.InsertMany(ctx,orderItemsToBeInserted)
+
+		if err!=nil{
+			log.Fatal(err)
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK,insertedOrderItems)
 	}
 }
 
 func UpdateOrderItem() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		ctx,cancel := context.WithTimeout(context.Background(),100*time.Second)
 		
+		var orderItem model.OrderItem
+		orderItemId := c.Param("order_item_id")
+		fliter := bson.M{"order_item_id":orderItemId}
+
+		var updateObj primitive.D
+		if orderItem.Unit_price !=nil{
+			updateObj = append(updateObj, bson.E{"unit_price",&*orderItem.Unit_price})
+		}
+		orderItem.Quantity !=nil{
+			updateObj = append(updateObj , bson.E{"quantity",*orderItem.Quantity})
+		}
+		orderItem.Food_id!=nil{
+			updateObj = append(updateObj, bson.E{"food_id", *orderItem.Food_id})
+		}
+
+		orderItem.Updated_at,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", orderItem.Updated_at})
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		result,err := orderItemCollection.UpdateOne(
+			ctx,
+			fliter,
+			bson.D{
+				{"$set", updateObj}
+			},
+			&opt,
+		)
+
+		if err!=nil{
+			msg := "order item update failed"
+			c.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+			return
+		}
+
+		defer cancel()
+
+		c.JSON(http.StatusOK,result)
 	}
 }
 
@@ -82,7 +157,7 @@ func ItemsByOrder(id string)(OrderItems []primitive.M,err error){
 	
 }
 
-func GetOrderItemsByOrder() gin.HandlerFunc{
+func GetOrderItemsByOrder()  	gin.HandlerFunc{
 	return func(c *gin.Context) {
 		orderId := c.Param("order_id")
 		allOrderItems,err := ItemsByOrder(orderId)
