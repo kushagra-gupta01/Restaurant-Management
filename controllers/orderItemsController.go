ada6d4fee0009354717bcb0f2c27ef72f8d8b66c
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-11-07 17:08:00 +0530

    Items by order function done!

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"time"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"github.com/kushagra-gupta01/Restaurant-Management/database"
 	"github.com/kushagra-gupta01/Restaurant-Management/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -117,10 +116,10 @@ func UpdateOrderItem() gin.HandlerFunc{
 		if orderItem.Unit_price !=nil{
 			updateObj = append(updateObj, bson.E{"unit_price",&*orderItem.Unit_price})
 		}
-		orderItem.Quantity !=nil{
+		if orderItem.Quantity !=nil{
 			updateObj = append(updateObj , bson.E{"quantity",*orderItem.Quantity})
 		}
-		orderItem.Food_id!=nil{
+		if orderItem.Food_id!=nil{
 			updateObj = append(updateObj, bson.E{"food_id", *orderItem.Food_id})
 		}
 
@@ -136,7 +135,7 @@ func UpdateOrderItem() gin.HandlerFunc{
 			ctx,
 			fliter,
 			bson.D{
-				{"$set", updateObj}
+				{"$set", updateObj},
 			},
 			&opt,
 		)
@@ -154,7 +153,73 @@ func UpdateOrderItem() gin.HandlerFunc{
 }
 
 func ItemsByOrder(id string)(OrderItems []primitive.M,err error){
+	ctx,cancel := context.WithTimeout(context.Background(),100*time.Second)
 	
+	matchStage := bson.D{{"$match",bson.D{{"_id",id}}}}
+	lookupStage := bson.D{{"$lookup",bson.D{{"from","food"},{"localField","food_id"},{"foreignField","food_id"},{"as","food"}}}}
+	unwindStage := bson.D{{"unwind",bson.D{{"path","$food"},{"preserveNullAndEmptyArrays",true}}}}
+
+	lookupOrderStage := bson.D{{"$lookup",bson.D{{"from","order"},{"localField","order_id"},{"foreignField","order_id"},{"as","order"}}}}
+	unwindOrderStage := bson.D{{"$unwind",bson.D{{"path","$order"},{"preserveNullAndEmptyArrays",true}}}}
+
+	lookupTableStage := bson.D{{"$lookup",bson.D{{"from","table"},{"localField","order.table_id"},{"foreignField","table_id"},{"as","table"}}}}
+	unwindTableStage := bson.D{{"$unwind",bson.D{{"path","$table"},{"preserveNullAndEmptyArrays",true}}}}
+
+	projectStage := bson.D{
+		{
+			"$project",bson.D{
+				{"_id",0},
+				{"amount","$food.price"},
+				{"total_count",1},
+				{"food_name","$food.food_name"},
+				{"food_image","$food.food_image"},
+				{"table_number","$table.table_number"},
+				{"table_id","$table.table_id"},
+				{"order_id","$order.order_id"},
+				{"price","$food.price"},
+				{"quantity",1},
+			},
+		},
+	}
+
+	groupStage := bson.D{{"$group",bson.D{{"order_id","$order_id"},{"table_id","$table_id"},{"table_number","$table_number"}}},{"payment_due",bson.D{{"$sum","$amount"}}},{"$total_count",bson.D{{"$sum",1}}},{"order_items",bson.D{{"$push","$$ROOT"}}}}
+
+	projectStage2 := bson.D{
+		{
+			"$project",bson.D{
+				{"_id",0},
+				{"payment_due",1},
+				{"total_count",1},
+				{"table_number","$_id.table_number"},
+				{"order_items",1},
+			},
+		},
+	}
+
+	result,err := orderItemCollection.Aggregate(ctx,mongo.Pipeline{
+		matchStage,
+		lookupStage,
+		unwindStage,
+		lookupOrderStage,
+		unwindOrderStage,
+		lookupTableStage,
+		unwindTableStage,
+		projectStage,
+		groupStage,
+		projectStage2,
+	})
+
+	if err!=nil{
+		panic(err)
+	}
+
+	if err = result.All(ctx,&OrderItems);err!=nil{
+		panic(err)
+	}
+
+	defer cancel()
+
+	return OrderItems,err
 }
 
 func GetOrderItemsByOrder()  	gin.HandlerFunc{
